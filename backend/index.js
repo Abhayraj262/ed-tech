f6db24ee72331fb61961189c89c0bbbc0a04a179
Abhayraj262 <abhayrajkashyap2002@gmail.com>
2025-02-22 14:57:15 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express";
-const app = express();
-
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
 dotenv.config();
 
+
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express());
+app.use(express.json());
+app.use(cookieParser());
+
+
 
 app.get("/", (req, res) => {
   res.status(200).json({
